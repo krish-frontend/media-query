5ff2334fcfcbf0227ae362d7cb73cf6cdc828457
Krissh <krrishkumar610@gmail.com>
2026-03-01 14:10:15 +0530

    linking

---
+++ my_vlog/index.js
@@ -0,0 +1,24 @@
+
+// const lines=[
+//    " Tu agar meri, yeh hawayein teri", "Tu agar meri, saari raahein teri",
+
+// "Tu agar meri, main hoon tera", "Tu agar meri, yeh ujaale tere",
+
+// "Tu agar meri, dil hawale tere", "Tu agar meri, main hoon tera",
+
+// "Betaab-sa mohabbat ka tu inqalaab hai","Mera jahaan teri baahon mein khwaab, khwaab hai",
+
+// "Gehra hua, gehra hua", "Rang aashiqui gehra hua",
+// ]          
+
+// let i=0;
+
+// function printLines(){
+//     if(i<lines.length){
+//         console.log(lines[i]);
+//         i++;
+        
+//         setTimeout(printLines, 2000)
+//     }
+// }
+// printLines()
